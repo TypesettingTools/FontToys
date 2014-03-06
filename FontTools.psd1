3f1721db0400ae912fc85a4673c6063562abb0eb
line0 <line0@mail.lv>
2014-03-06 05:47:23 +0100

    initial commit

---
+++ FontTools.psd1
@@ -0,0 +1,20 @@
+﻿@{
+RootModule = 'FontTools.psm1'
+ModuleVersion = '0.0.1'
+GUID = '0544c2df-1a5e-4da7-95c8-1583af4b5b9b'
+Author = 'line0'
+Description = 'A small set of PowerShell modules that enable batch processing of Matroska files with mkvtoolnix.'
+
+PowerShellVersion = '3.0'
+
+NestedModules = @('Import-Font.psm1', 'Export-Font.psm1', 'Format-Font.psm1')
+FunctionsToExport = @('Import-Font', 'Format-Font', 'Export-Font')
+CmdletsToExport = ''
+VariablesToExport = '*'
+AliasesToExport = '*'
+RequiredModules = @('MkvTools')
+
+
+# HelpInfo URI of this module
+# HelpInfoURI = ''
+}
---
+++ Types.ps1
@@ -0,0 +1,47 @@
+﻿Add-Type -TypeDefinition @"
+   [System.Flags]
+   public enum fsSelection
+   {
+      Invalid = 0,
+      Italic = 1,
+      Underscore = 2,
+      Negative = 4,
+      Outlined = 8,
+      Strikeout = 16,
+      Bold = 32,
+      Regular = 64,
+      UseTypoMetrics = 128,
+      WWS = 256,
+      Oblique = 512
+   }
+"@
+
+Add-Type -TypeDefinition @"
+   public enum usWeightClass
+   {
+      Thin = 100,
+      ExtraLight = 200,
+      Light = 300,
+      Regular = 400,
+      Medium = 500,
+      SemiBold = 600,
+      Bold = 700,
+      ExtraBold = 800,
+      Black = 900
+   }
+"@
+
+Add-Type -TypeDefinition @"
+    public enum usWidthClass
+    {
+        UltraCondensed = 1,
+        ExtraCondensed = 2,
+        Condensed = 3,
+        SemiCondensed = 4,
+        Normal = 5,
+        SemiExpanded = 6,
+        Expanded = 7,
+        ExtraExpanded = 8,
+        UltraExpanded = 9
+    }
+"@
